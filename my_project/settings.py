420ca6ae8dfa9d3a21226eda65a5ebed3fabe881
vamshigurrala217-lgtm <vamshigurrala217@gmail.com>
2026-09-05 15:14:46 +0530

    allowed

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qi(zss1b&7=mqm1$9)%iu!&bw5x7&2w0cbi8r)qrj4ek-gi50&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ['.onrender.com', 'https://food-eight-woad.vercel.app/']
 
 
 # Application definition
